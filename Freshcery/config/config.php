9a2b7f3e6f284730450f8dc0a17922eba3de072b
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-03-05 00:24:24 +0200

    registering users

---
+++ Freshcery/config/config.php
@@ -0,0 +1,35 @@
+<?php
+
+if(isset($_SERVER['HTTP_REFERER'])){
+    //redirect them to you desired location
+    header('location: index.php');
+    exit;
+}
+
+
+
+try{
+//host
+define("HOST","localhost");
+
+//dbname
+define("DBNAME", "online-shop");
+
+//user
+define("USER","root");
+
+//pass
+define("PASS","");
+
+
+$conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME.";", USER, PASS);
+$conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// if($conn == true) {
+//     echo "Success";
+// } else {
+//     echo "error";
+// }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
