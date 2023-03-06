6eda20b90e9c94aabdfe26227d899e0a32620e25
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-03-06 21:45:46 +0200

    adding logic to the login page

--- Freshcery/auth/register.php
+++ Freshcery/auth/register.php
@@ -3,102 +3,104 @@
 
 <?php
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
 
-    if(empty($_POST['fullname']) OR empty($_POST['email']) OR empty($_POST['username']) 
-    OR empty($_POST['password'])) {
+    if (
+        empty($_POST['fullname']) or empty($_POST['email']) or empty($_POST['username'])
+        or empty($_POST['password'])
+    ) {
 
-    echo "<script>alert('one or more inputs are empty');<script>";
-   } else{
+        echo "<script>alert('one or more inputs are empty');<script>";
+    } else {
 
-     if($_POST['password'] == $_POST['confirm_password']){
-        $fullname = $_POST['fullname'];
-        $email = $_POST['email'];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $img = "user.png";
+        if ($_POST['password'] == $_POST['confirm_password']) {
+            $fullname = $_POST['fullname'];
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $img = "user.png";
 
-        $insert = $conn->prepare("INSERT INTO users(fullname, email, username, mypassword, image)
+            $insert = $conn->prepare("INSERT INTO users(fullname, email, username, mypassword, image)
          VALUES(:fullname, :email, :username, :mypassword, :image)");
 
-         $insert->execute([
-            ":fullname" => $fullname,
-            ":email" => $email,
-            ":username" => $username,
-            ":mypassword" => password_hash($mypassword, PASSWORD_DEFAULT),
-            ":image" => $image
-         ]);
+            $insert->execute([
+                ":fullname" => $fullname,
+                ":email" => $email,
+                ":username" => $username,
+                ":mypassword" => password_hash($password, PASSWORD_DEFAULT),
+                ":image" => $img
+            ]);
 
-         header("location: ".APPURL."/login.php");
+            //  header("location: login.php");
 
-       //  $confirmpassword = $_POST[''];
-     } else {
-        echo "<script>alert('password does not match');<script>";
-     }
+            echo "<script> window.location.href='login.php'; </script>";
 
-    
-   }
+            //  $confirmpassword = $_POST[''];
+        } else {
+            echo "<script>alert('password does not match');<script>";
+        }
+    }
 }
 
 ?>
-    <div id="page-content" class="page-content">
-        <div class="banner">
-            <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL; ?>/assets/img/bg-header.jpg');">
-                <div class="container">
-                    <h1 class="pt-5">
-                        Register Page
-                    </h1>
-                    <p class="lead">
-                        Save time and leave the groceries to us.
-                    </p>
+<div id="page-content" class="page-content">
+    <div class="banner">
+        <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL; ?>/assets/img/bg-header.jpg');">
+            <div class="container">
+                <h1 class="pt-5">
+                    Register Page
+                </h1>
+                <p class="lead">
+                    Save time and leave the groceries to us.
+                </p>
 
-                    <div class="card card-login mb-5">
-                        <div class="card-body">
-                            <form class="form-horizontal" method="POST" action="register.php">
-                                <div class="form-group row mt-3">
-                                    <div class="col-md-12">
-                                        <input class="form-control" name="fullname" type="text" required="" placeholder="Full Name">
-                                    </div>
+                <div class="card card-login mb-5">
+                    <div class="card-body">
+                        <form class="form-horizontal" method="POST" action="register.php">
+                            <div class="form-group row mt-3">
+                                <div class="col-md-12">
+                                    <input class="form-control" name="fullname" type="text" required="" placeholder="Full Name">
                                 </div>
-                                <div class="form-group row mt-3">
-                                    <div class="col-md-12">
-                                        <input class="form-control" type="email" required="" placeholder="Email">
-                                    </div>
+                            </div>
+                            <div class="form-group row mt-3">
+                                <div class="col-md-12">
+                                    <input class="form-control" name="email" type="email" required="" placeholder="Email">
                                 </div>
-                                
-                                <div class="form-group row mt-3">
-                                    <div class="col-md-12">
-                                        <input class="form-control" type="text" required="" placeholder="Username">
-                                    </div>
+                            </div>
+
+                            <div class="form-group row mt-3">
+                                <div class="col-md-12">
+                                    <input class="form-control" name="username" type="text" required="" placeholder="Username">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-md-12">
-                                        <input class="form-control" type="password" required="" placeholder="Password">
-                                    </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input class="form-control" name="password" type="password" required="" placeholder="Password">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-md-12">
-                                        <input class="form-control" type="password" required="" placeholder="Confirm Password">
-                                    </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input class="form-control" name="confirm_password" type="password" required="" placeholder="Confirm Password">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-md-12">
+                            </div>
+                            <div class="form-group row">
+                                <!-- <div class="col-md-12">
                                         <div class="checkbox">
                                             <input id="checkbox0" type="checkbox" name="terms">
                                             <label for="checkbox0" class="mb-0">I Agree with <a href="terms.html" class="text-light">Terms & Conditions</a> </label>
                                         </div>
-                                    </div>
+                                    </div> -->
+                            </div>
+                            <div class="form-group row text-center mt-4">
+                                <div class="col-md-12">
+                                    <button type="submit" name="submit" class="btn btn-primary btn-block text-uppercase">Register</button>
                                 </div>
-                                <div class="form-group row text-center mt-4">
-                                    <div class="col-md-12">
-                                        <button type="submit" class="btn btn-primary btn-block text-uppercase">Register</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <?php require "../includes/footer.php"; ?> 
+</div>
+<?php require "../includes/footer.php"; ?>
