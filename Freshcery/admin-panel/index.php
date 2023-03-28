650522498c1b71c270a5376b5c5ea76f29e434cb
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-03-28 21:16:51 +0200

    showing categories

--- Freshcery/admin-panel/index.php
+++ Freshcery/admin-panel/index.php
@@ -7,10 +7,30 @@ if (!isset($_SESSION['adminname'])) {
   echo "<script> window.location.href='".ADMINURL."/admins/login-admins.php'; </script>";
 }
 
+// products
 $products = $conn->query("SELECT COUNT(*) as products_num FROM products");
 $products->execute();
 
 $num_products = $products->fetch(PDO::FETCH_OBJ);
+
+// orders
+$orders = $conn->query("SELECT COUNT(*) as orders_num FROM orders");
+$orders->execute();
+
+$num_orders = $orders->fetch(PDO::FETCH_OBJ);
+
+// categories
+$categories = $conn->query("SELECT COUNT(*) as categories_num FROM categories");
+$categories->execute();
+
+$num_categories = $categories->fetch(PDO::FETCH_OBJ);
+
+// admins
+$admins = $conn->query("SELECT COUNT(*) as admins_num FROM admins");
+$admins->execute();
+
+$num_admins = $admins->fetch(PDO::FETCH_OBJ);
+
 
 ?>
             
@@ -30,7 +50,7 @@ $num_products = $products->fetch(PDO::FETCH_OBJ);
             <div class="card-body">
               <h5 class="card-title">Orders</h5>
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of orders: 8</p>
+              <p class="card-text">number of orders: <?php echo $num_orders->orders_num; ?></p>
              
             </div>
           </div>
@@ -40,7 +60,7 @@ $num_products = $products->fetch(PDO::FETCH_OBJ);
             <div class="card-body">
               <h5 class="card-title">Categories</h5>
               
-              <p class="card-text">number of categories: 4</p>
+              <p class="card-text">number of categories: <?php echo $num_categories->categories_num; ?></p>
               
             </div>
           </div>
@@ -50,7 +70,7 @@ $num_products = $products->fetch(PDO::FETCH_OBJ);
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 3</p>
+              <p class="card-text">number of admins: <?php echo $num_admins->admins_num; ?></p>
               
             </div>
           </div>
