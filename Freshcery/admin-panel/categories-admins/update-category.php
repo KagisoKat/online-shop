83dccade695686d88290138f258b1b92d6443829
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-03-30 17:31:18 +0200

    updating categories

--- Freshcery/admin-panel/categories-admins/update-category.php
+++ Freshcery/admin-panel/categories-admins/update-category.php
@@ -7,6 +7,13 @@ if (!isset($_SESSION['adminname'])) {
   echo "<script> window.location.href='" . ADMINURL . "/admins/login-admins.php'; </script>";
 }
 
+if(isset($_GET['id'])) {
+
+  $id = $_GET['id'];
+
+  $select = $conn->query("SELECT * FROM categories WHERE id='$id");
+  $select->execute();
+}
 
 // if (isset($_POST['submit'])) {
 
