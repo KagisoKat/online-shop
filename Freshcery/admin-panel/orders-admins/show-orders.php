f4d8fea20e81a16160b02c81e16bf4b57c1ad9bf
KagisoKat <110462927+KagisoKat@users.noreply.github.com>
2023-03-31 17:28:01 +0200

    updating orders

--- Freshcery/admin-panel/orders-admins/show-orders.php
+++ Freshcery/admin-panel/orders-admins/show-orders.php
@@ -15,47 +15,48 @@ $allOrders = $orders->fetchAll(PDO::FETCH_OBJ);
 
 
 ?>
-    <div class="row">
-        <div class="col">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title mb-4 d-inline">Orders</h5>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">first name</th>
-                    <th scope="col">last name</th>
-                    <th scope="col">email</th>
-                    <th scope="col">country</th>
-                    <th scope="col">status</th>
-                    <th scope="col">price in USD</th>
-                    <th scope="col">date</th>
-                    <th scope="col">update</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php foreach($allOrders as $order) : ?>
-                  <tr>
-                    <th scope="row"><?php echo $order->id; ?></th>
-                    <td><?php echo $order->name; ?></td>
-                    <td><?php echo $order->lname; ?></td>
-                    <td><?php echo $order->email; ?></td>
-                    <td><?php echo $order->country; ?></td>
-                    <td><?php echo $order->status; ?></td>
-                    <td><?php echo $order->price; ?></td>
-                    <td><?php echo $order->created_at; ?></td>
-                    <td>                
-                        <a href="#" class="btn btn-warning text-white mb-4 text-center">update</a>
-                    </td>
-                   
-                  </tr>
-                </tbody>
-              </table> 
-            </div>
-          </div>
-        </div>
+<div class="row">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title mb-4 d-inline">Orders</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">first name</th>
+              <th scope="col">last name</th>
+              <th scope="col">email</th>
+              <th scope="col">country</th>
+              <th scope="col">status</th>
+              <th scope="col">price in USD</th>
+              <th scope="col">date</th>
+              <th scope="col">update</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($allOrders as $order) : ?>
+              <tr>
+                <th scope="row"><?php echo $order->id; ?></th>
+                <td><?php echo $order->name; ?></td>
+                <td><?php echo $order->lname; ?></td>
+                <td><?php echo $order->email; ?></td>
+                <td><?php echo $order->country; ?></td>
+                <td><?php echo $order->status; ?></td>
+                <td><?php echo $order->price; ?></td>
+                <td><?php echo $order->created_at; ?></td>
+                <td>
+                  <a href="<?php echo ADMINURL; ?>/orders-admins/update-orders.php?id=<?php echo $order->id; ?>" class="btn btn-warning text-white mb-4 text-center">update</a>
+                </td>
+
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
       </div>
+    </div>
+  </div>
+</div>
 
 
-      <?php require "../layouts/footer.php"; ?>
+<?php require "../layouts/footer.php"; ?>
---
+++ Freshcery/admin-panel/orders-admins/update-orders.php
@@ -0,0 +1,90 @@
+<?php require "../layouts/header.php"; ?>
+<?php require "../../config/config.php"; ?>
+
+<?php
+
+if (!isset($_SESSION['adminname'])) {
+  echo "<script> window.location.href='" . ADMINURL . "/admins/login-admins.php'; </script>";
+}
+
+if(isset($_GET['id'])) {
+
+
+  $id = $_GET['id'];
+
+  $select = $conn->query("SELECT * FROM orders WHERE id='$id'");
+  $select->execute();
+
+  $order = $select->fetch(PDO::FETCH_OBJ);
+
+//   if (isset($_POST['submit'])) {
+
+//     if (
+//       (empty($_POST['name'])) or empty($_POST['icon']) or empty($_POST['description'])
+//     ) {
+  
+//       echo "<script>alert('one or more inputs are empty');</script>";
+//     } else {
+  
+  
+  
+//       $name = $_POST['name'];
+//       $icon = $_POST['icon'];
+//       $description = $_POST['description'];
+     
+  
+//       // var_dump($email);
+//       // var_dump($adminname);
+//       // var_dump($password);
+  
+     
+  
+//       $insert = $conn->prepare("UPDATE categories SET name = :name, icon = :icon, description = :description  WHERE id='$id'");
+  
+//       $insert->execute([
+//         ":name" => $name,
+//         ":icon" => $icon,
+//         ":description" => $description,
+//       ]);
+
+//       echo "<script> window.location.href='" . ADMINURL . "/categories-admins/show-categories.php'; </script>";
+  
+  
+//     }
+//   }
+}
+
+
+?>
+
+
+<div class="row">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title mb-5 d-inline">Update Order</h5>
+        <form method="POST" action="update-category.php?id=<?php echo $id; ?>">
+          <!-- Email input -->
+          <div class="form-outline mb-4 mt-4">
+            <input type="text" name="name" id="form2Example1" value="<?php echo $category->name; ?>" class="form-control" placeholder="name" />
+          </div>
+          <div class="form-outline mb-4 mt-4">
+            <input type="text" name="icon" id="form2Example1"<?php echo $category->icon; ?> class="form-control" placeholder="icon" />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Description</label>
+            <textarea name="description" placeholder="description" class="form-control" id="exampleFormControlTextarea1" rows="3"><?php echo $category->description; ?></textarea>
+          </div>
+          <!-- Submit button -->
+          <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Update</button>
+
+
+        </form>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<?php require "../layouts/footer.php"; ?>
